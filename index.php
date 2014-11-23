57b68930f5a02dd60ffc25e1b8c892276eabe2b1
Vidit Tiwari <vidittiwari95@gmail.com>
2014-11-23 22:33:44 +0530

    php script
    It is a php script embedded in html file to delete numbers and extra
    spaces from a text file and produce a output file.

---
+++ index.php
@@ -0,0 +1,38 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<?php
+
+$file_handle = fopen("input.txt", "r");
+
+$file_handle1 = fopen("output1.txt", "w");
+$ctr=0;
+while (!feof($file_handle)) {
+	if($ctr>0)
+	{
+		fwrite($file_handle1, "\n");
+	}
+   $line = fgets($file_handle);
+   $line = preg_replace('/[0-9]/', "", $line);
+   $line = ltrim($line);
+   $line = preg_replace('/\s+/', ' ', $line);
+   
+
+   echo $line;
+   echo '<br>';
+   fwrite($file_handle1, $line) or die("Could not write to file");
+  $ctr++;
+   
+   
+}
+fclose($file_handle);
+fclose($file_handle1);
+
+
+?>
+
+</body>
+</html>
