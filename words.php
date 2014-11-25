961f31b90bbcfcb811896b222b23018a91e9cbc6
Vidit Tiwari <vidittiwari95@gmail.com>
2014-11-25 21:10:40 +0530

    newline-fix-in-words.php

--- words.php
+++ words.php
@@ -9,19 +9,16 @@
 $file_handle = fopen("words_input.txt", "r");
 
 $file_handle1 = fopen("words_output.txt", "w");
-$ctr=0;
+
 while (!feof($file_handle)) {
-	if($ctr>0)
-	{
-		fwrite($file_handle1, "\n");
-	}
+	
    $line = fgets($file_handle);
    $line = preg_replace('/([|].*)/', "", $line);
    
    echo $line;
    echo '<br>';
    fwrite($file_handle1, $line) or die("Could not write to file");
-  $ctr++;
+  
    
    
 }
